0f767584a80fc74a538153a7c1ff3914aae88ecc
aric-1 <156359530+aric-1@users.noreply.github.com>
2024-02-01 16:43:12 -0800

    Debugged array methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void myTestReversed() {
+    int[] input1 = {1, 2};
+    assertArrayEquals(new int[]{2, 1}, ArrayExamples.reversed(input1));
+  }
+
+	@Test 
+	public void myTestReverseInPlace() {
+    int[] input1 = {  4, 3, 2, 1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1, 2, 3, 4 }, input1);
+	}
 }
